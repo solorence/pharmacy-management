944b95ab3f1787b413f432c6a0cc67ed06ebe477
solorence <doc.electro1@gmail.com>
2026-06-11 17:23:28 +0100

    major readjustment. main content area adjusted to have a header and a body. the nav contents are mdisplayed in the body of the main area

--- js/app.js
+++ js/app.js
@@ -2,27 +2,27 @@ window.onload = gotohome;
 
 function gotohome(){
 	//window.location.href='index.html';
-	fetch('sidebar/dashboard.html', {cache:'no-store'}).then(res=> res.text()).then(html=>document.getElementById('content').innerHTML = html);
+	fetch('sidebar/dashboard.html', {cache:'no-store'}).then(res=> res.text()).then(html=>document.getElementById('main-content').innerHTML = html);
 }
 
 
 function gotosales(){
-	fetch('sidebar/sales.html').then(res=>res.text()).then(html=>document.getElementById('content').innerHTML = html)
+	fetch('sidebar/sales.html').then(res=>res.text()).then(html=>document.getElementById('main-content').innerHTML = html)
 }
 
 
 function gotoinventory(){
-	fetch('sidebar/inventory.html').then(res=>res.text()).then(html=>document.getElementById('content').innerHTML = html)
+	fetch('sidebar/inventory.html').then(res=>res.text()).then(html=>document.getElementById('main-content').innerHTML = html)
 }
 
 
 function gotopurchase(){
-	fetch('sidebar/purchase.html').then(res=>res.text()).then(html=>document.getElementById('content').innerHTML = html)
+	fetch('sidebar/purchase.html').then(res=>res.text()).then(html=>document.getElementById('main-content').innerHTML = html)
 }
 
 
 function gotoreports(){
-	fetch('sidebar/reports.html').then(res=>res.text()).then(html=>document.getElementById('content').innerHTML = html)
+	fetch('sidebar/reports.html').then(res=>res.text()).then(html=>document.getElementById('main-content').innerHTML = html)
 }
 
 
