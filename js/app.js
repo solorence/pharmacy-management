294f60e352e21230526fd24f6928661e310dffd0
solorence <doc.electro1@gmail.com>
2026-06-11 15:39:23 +0100

    major change happened here. The index.html/dashboard has the major content. Other pages are added to the index.html page using fetch() function. used http://127.0.0.1:8080 to server the page better to ignore caching effectively after adding {cache:'no-store'} as the 2nd argument to the fetch function and running the node server with this command: 'http-server -c-1'

--- js/app.js
+++ js/app.js
@@ -1,7 +1,32 @@
+window.onload = gotohome;
+
 function gotohome(){
-	window.location.href='../index.html';
+	//window.location.href='index.html';
+	fetch('sidebar/dashboard.html', {cache:'no-store'}).then(res=> res.text()).then(html=>document.getElementById('content').innerHTML = html);
 }
+
+
+function gotosales(){
+	fetch('sidebar/sales.html').then(res=>res.text()).then(html=>document.getElementById('content').innerHTML = html)
+}
+
+
+function gotoinventory(){
+	fetch('sidebar/inventory.html').then(res=>res.text()).then(html=>document.getElementById('content').innerHTML = html)
+}
+
+
+function gotopurchase(){
+	fetch('sidebar/purchase.html').then(res=>res.text()).then(html=>document.getElementById('content').innerHTML = html)
+}
+
+
+function gotoreports(){
+	fetch('sidebar/reports.html').then(res=>res.text()).then(html=>document.getElementById('content').innerHTML = html)
+}
+
 
 function gotoself(){
 	window.location.href='index.html'	
 }
+console.log('test cache! hope it works well now')
