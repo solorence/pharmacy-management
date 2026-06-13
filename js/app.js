827fab0dbe27ae503538c44ec854ee414302b568
solorence <doc.electro1@gmail.com>
2026-06-13 13:36:38 +0100

    major change in the nav area with the creation  of nav look 2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 window.onload = gotohome;
 
+let main_nav_active = true;
+
 function gotohome(){
 	//window.location.href='index.html';
 	fetch('sidebar/dashboard.html', {cache:'no-store'}).then(res=> res.text()).then(html=>document.getElementById('main-content').innerHTML = html);
@@ -7,20 +9,47 @@ function gotohome(){
 
 
 function gotosales(){
-	fetch('sidebar/sales.html').then(res=>res.text()).then(html=>document.getElementById('main-content').innerHTML = html)
+	fetch('sidebar/sales.html').then(res=>res.text()).then(html=>document.getElementById('main-content').innerHTML = html);
 }
 
 
 function gotoinventory(){
-	fetch('sidebar/inventory.html').then(res=>res.text()).then(html=>document.getElementById('main-content').innerHTML = html)
+	fetch('sidebar/inventory.html').then(res=>res.text()).then(html=>document.getElementById('main-content').innerHTML = html);
 }
 
 
 function gotopurchase(){
-	fetch('sidebar/purchase.html').then(res=>res.text()).then(html=>document.getElementById('main-content').innerHTML = html)
+	fetch('sidebar/purchase.html').then(res=>res.text()).then(html=>document.getElementById('main-content').innerHTML = html);
 }
 
 
 function gotoreports(){
-	fetch('sidebar/reports.html').then(res=>res.text()).then(html=>document.getElementById('main-content').innerHTML = html)
+	fetch('sidebar/reports.html').then(res=>res.text()).then(html=>document.getElementById('main-content').innerHTML = html);
+}
+
+function togglenav(){
+	main_nav_active ? loadnav2() : loadnav1();
+}
+
+
+function loadnav1(){
+	fetch('sidebar/navlist1.html').then(res=>res.text()).then(html=>document.getElementById('nav-container').innerHTML = html);
+	resizenav1();
+	main_nav_active=true;
+}
+
+function loadnav2(){
+	fetch('sidebar/navlist2.html').then(res=>res.text()).then(html=>document.getElementById('nav-container').innerHTML = html);
+	resizenav2();
+	main_nav_active=false;
+}
+
+function resizenav1(){
+	let nav1 = document.getElementById('nav-container');
+	nav1.style.minWidth='16vw';
+}
+
+function resizenav2(){
+	let nav2 = document.getElementById('nav-container');
+	nav2.style.minWidth='4vw';	
 }
