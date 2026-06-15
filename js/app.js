1b1cdaa9fe750ce3dbe7b839166f08d7c1735f8b
solorence <doc.electro1@gmail.com>
2026-06-15 17:35:30 +0100

    changed what happens to the link background color when the link is clicked. that is  the present link button is highlited when its corresponding page is active

--- js/app.js
+++ js/app.js
@@ -2,29 +2,54 @@ window.onload = gotohome;
 
 let main_nav_active = true;
 
-function gotohome(){
+let main_c = document.getElementById('main-content');
+
+let new_active_page;
+let old_active_page;
+
+function gotohome(event){
+	old_active_page = new_active_page;
+	new_active_page = event.currentTarget;
+
+	new_active_page.style.backgroundColor = '#84FF6F';
+	new_active_page.style.color = 'black';
+
+	if (new_active_page !== old_active_page && old_active_page != undefined) {
+		old_active_page.style.backgroundColor = '#38B000';
+		old_active_page.style.color = 'white';
+	}
 	//window.location.href='index.html';
-	fetch('sidebar/dashboard.html', {cache:'no-store'}).then(res=> res.text()).then(html=>document.getElementById('main-content').innerHTML = html);
+	fetch('sidebar/dashboard.html', {cache:'no-store'}).then(res=> res.text()).then(html=>main_c.innerHTML = html);
 }
 
 
-function gotosales(){
-	fetch('sidebar/sales.html').then(res=>res.text()).then(html=>document.getElementById('main-content').innerHTML = html);
+function gotosales(event){
+	old_active_page = new_active_page;
+	new_active_page = event.currentTarget;
+	
+	new_active_page.style.backgroundColor = '#84FF6F';
+	new_active_page.style.color = 'black';
+
+	if (new_active_page !== old_active_page && old_active_page != undefined) {
+		old_active_page.style.backgroundColor = '#38B000';
+		old_active_page.style.color = 'white';
+	}
+	fetch('sidebar/sales.html').then(res=>res.text()).then(html=>main_c.innerHTML = html);
 }
 
 
 function gotoinventory(){
-	fetch('sidebar/inventory.html').then(res=>res.text()).then(html=>document.getElementById('main-content').innerHTML = html);
+	fetch('sidebar/inventory.html').then(res=>res.text()).then(html=>main_c.innerHTML = html);
 }
 
 
 function gotopurchase(){
-	fetch('sidebar/purchase.html').then(res=>res.text()).then(html=>document.getElementById('main-content').innerHTML = html);
+	fetch('sidebar/purchase.html').then(res=>res.text()).then(html=>main_c.innerHTML = html);
 }
 
 
 function gotoreports(){
-	fetch('sidebar/reports.html').then(res=>res.text()).then(html=>document.getElementById('main-content').innerHTML = html);
+	fetch('sidebar/reports.html').then(res=>res.text()).then(html=>main_c.innerHTML = html);
 }
 
 function togglenav(){
